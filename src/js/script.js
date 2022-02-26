9dc6b75f6cf9b9de8f4cbcc1e4759980e1c8b398
nokz00 <nomarkhanov@gmail.com>
2022-02-26 21:54:34 +0600

    first app

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже помотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов", ""),
+      b = prompt("Насколько оцените его", ""),
+      c = prompt("Один из последних просмотренных фильмов", ""),
+      d = prompt("Насколько оцените его", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
